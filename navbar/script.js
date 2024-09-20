07df7cbda628b564f8fe73b03b4a182dcfbd8a12
vinith579 <vinithvinith356140@gmail.com>
2024-09-21 00:02:22 +0530

    first commit

---
+++ navbar/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// Change active status dynamically when clicking links
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+        navLinks.forEach(nav => nav.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
